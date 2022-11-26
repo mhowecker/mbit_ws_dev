9f8f51e41de62b03fc3fd57396706b5b859b3e7f
mhowecker-tgm <mhowecker@student.tgm.ac.at>
2022-11-26 16:13:24 +0100

    python tf2 broadcast finished

--- src/learning_tf2_py/setup.py
+++ src/learning_tf2_py/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'learning_tf2_py'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'static_turtle_tf2_broadcaster = learning_tf2_py.static_turtle_tf2_broadcaster:main',
+            'turtle_tf2_broadcaster = learning_tf2_py.turtle_tf2_broadcaster:main',
         ],
     },
 )
